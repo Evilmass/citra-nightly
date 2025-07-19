c26a66509be1fc49bad1e34740443a09b6eecab5
Evilmass <21218296+Evilmass@users.noreply.github.com>
2025-07-19 17:14:52 +0800

    update

--- CMakeModules/DownloadExternals.cmake
+++ CMakeModules/DownloadExternals.cmake
@@ -7,9 +7,9 @@ function(download_bundled_external remote_path lib_name prefix_var)
 set(prefix "${CMAKE_BINARY_DIR}/externals/${lib_name}")
 if (NOT EXISTS "${prefix}")
     message(STATUS "Downloading binaries for ${lib_name}...")
-    file(DOWNLOAD
-        https://github.com/citra-emu/ext-windows-bin/raw/master/${remote_path}${lib_name}.7z
-        "${CMAKE_BINARY_DIR}/externals/${lib_name}.7z" SHOW_PROGRESS)
+    #file(DOWNLOAD
+    #    https://github.com/yuzu-mirror/ext-windows-bin/raw/master/${remote_path}${lib_name}.7z
+    #    "${CMAKE_BINARY_DIR}/externals/${lib_name}.7z" SHOW_PROGRESS)
     execute_process(COMMAND ${CMAKE_COMMAND} -E tar xf "${CMAKE_BINARY_DIR}/externals/${lib_name}.7z"
         WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/externals")
 endif()
