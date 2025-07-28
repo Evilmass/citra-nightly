b130649610cc72c3f2cf51528fd12855b2e670de
Evilmass <Evilmass@users.noreply.github.com>
2025-07-28 16:47:44 +0800

    use powershell to build

--- .ci/windows-msvc/build.sh
+++ .ci/windows-msvc/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 
 mkdir build && cd build
-cmake .. -G "Visual Studio 17 2022" -A x64 -DCITRA_USE_BUNDLED_QT=1 -DCITRA_USE_BUNDLED_SDL2=1 -DCITRA_ENABLE_COMPATIBILITY_REPORTING=OFF -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=OFF -DUSE_DISCORD_PRESENCE=OFF -DENABLE_MF=ON -DENABLE_FFMPEG_VIDEO_DUMPER=ON
+cmake .. -G "Visual Studio 17 2022" -A x64 -DCITRA_USE_BUNDLED_QT=1 -DCITRA_USE_BUNDLED_SDL2=1 -DCITRA_ENABLE_COMPATIBILITY_REPORTING=OFF -DUSE_DISCORD_PRESENCE=OFF -DENABLE_MF=ON -DENABLE_FFMPEG_VIDEO_DUMPER=ON
 cd ..
 msbuild build/citra.sln -t:rebuild -verbosity:diag -property:Configuration=Release -property:Platform=x64
 ctest -VV -C Release || echo "::error ::Test error occurred on Windows MSVC build"
