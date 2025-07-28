0e196e83a7eb6734691c9268cb44b3cf9ca819b6
Evilmass <21218296+Evilmass@users.noreply.github.com>
2025-07-28 20:07:20 +0800

    fix windows build ninja build

--- .ci/windows.sh
+++ .ci/windows.sh
@@ -4,8 +4,6 @@ mkdir build && cd build
 cmake .. -Wno-dev -G "Visual Studio 17 2022" -A x64 \
     -DCMAKE_TOOLCHAIN_FILE="$(pwd)/../CMakeModules/MSVCCache.cmake" \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_COMPILER_LAUNCHER=ccache \
-    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
     -DCITRA_USE_BUNDLED_QT=1 \
     -DCITRA_USE_BUNDLED_SDL2=1 \
     -DENABLE_QT_TRANSLATION=ON \
@@ -15,5 +13,4 @@ cmake .. -Wno-dev -G "Visual Studio 17 2022" -A x64 \
     -DENABLE_FFMPEG_VIDEO_DUMPER=ON \
     -DENABLE_MF=ON
 msbuild build/citra.sln -t:rebuild -verbosity:diag -property:Configuration=Release -property:Platform=x64
-ccache -s -v
 ctest -VV -C Release || echo "::error ::Test error occurred on Windows build"
