7c7ce3e3a8a37521a15168c1b8c1dc251d6ebe7b
Evilmass <Evilmass@users.noreply.github.com>
2025-07-31 11:18:29 +0800

    use windows.sh

--- .ci/windows.sh
+++ .ci/windows.sh
@@ -3,17 +3,14 @@
 mkdir build && cd build
 # cmake .. -G "Visual Studio 15 2017 Win64" -A x64 \
 cmake .. -G "Visual Studio 17 2022" -A x64 \
-    -DCMAKE_SYSTEM_VERSION=10.0.19041.0 \
-    -DCMAKE_C_COMPILER="C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.44.35207\bin\HostX86\x64\cl.exe" \
-    -DCMAKE_CXX_COMPILER="C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.44.35207\bin\HostX86\x64\cl.exe" \
     -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
     -DCMAKE_BUILD_TYPE=Release \
     -DCITRA_USE_BUNDLED_QT=1 \
     -DCITRA_USE_BUNDLED_SDL2=1 \
     -DENABLE_QT_TRANSLATION=ON \
     -DCITRA_ENABLE_COMPATIBILITY_REPORTING=OFF \
-    -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON \
-    -DUSE_DISCORD_PRESENCE=ON \
+    -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=OFF \
+    -DUSE_DISCORD_PRESENCE=OFF \
     -DENABLE_FFMPEG_VIDEO_DUMPER=ON \
     -DENABLE_MF=ON
 cd ..
