d8522c1b829a962dc6e1219d4dd57584355e5d2e
Evilmass <21218296+Evilmass@users.noreply.github.com>
2025-07-31 20:21:09 +0800

    new pack.sh

--- .ci/pack.sh
+++ .ci/pack.sh
@@ -1,80 +1,48 @@
 #!/bin/bash -ex
 
-# Determine the full revision name.
 GITDATE="`git show -s --date=short --format='%ad' | sed 's/-//g'`"
 GITREV="`git show -s --format='%h'`"
-REV_NAME="citra-$OS-$TARGET-$GITDATE-$GITREV"
+REV_NAME="citra-${OS}-${TARGET}-${GITDATE}-${GITREV}"
 
-# Determine the name of the release being built.
+# Find out what release we are building
 if [[ "$GITHUB_REF_NAME" =~ ^canary- ]] || [[ "$GITHUB_REF_NAME" =~ ^nightly- ]]; then
     RELEASE_NAME=$(echo $GITHUB_REF_NAME | cut -d- -f1)
+    # For compatibility with existing installs, use mingw/osx in the archive and target names.
+    if [ "$TARGET" = "msys2" ]; then
+        REV_NAME="citra-${OS}-mingw-${GITDATE}-${GITREV}"
+        RELEASE_NAME="${RELEASE_NAME}-mingw"
+    elif [ "$OS" = "macos" ]; then
+        REV_NAME="citra-osx-${TARGET}-${GITDATE}-${GITREV}"
+    fi
 else
     RELEASE_NAME=head
 fi
 
-# Archive and upload the artifacts.
-mkdir artifacts
+mkdir -p artifacts
 
-function pack_artifacts() {
-    ARTIFACTS_PATH="$1"
-
-    # Set up root directory for archive.
+if [ -z "${UPLOAD_RAW}" ]; then
+    # Archive and upload the artifacts.
     mkdir "$REV_NAME"
-    if [ -f "$ARTIFACTS_PATH" ]; then
-        mv "$ARTIFACTS_PATH" "$REV_NAME"
+    mv build/bundle/* "$REV_NAME"
 
-        # Use file extension to differentiate archives.
-        FILENAME=$(basename "$ARTIFACT")
-        EXTENSION="${FILENAME##*.}"
-        ARCHIVE_NAME="$REV_NAME.$EXTENSION"
-    else
-        mv "$ARTIFACTS_PATH"/* "$REV_NAME"
-
-        ARCHIVE_NAME="$REV_NAME"
-    fi
-
-    # Create .zip/.tar.gz
     if [ "$OS" = "windows" ]; then
-        ARCHIVE_FULL_NAME="$ARCHIVE_NAME.zip"
-        # powershell Compress-Archive "$REV_NAME" "$ARCHIVE_FULL_NAME"
-    elif [ "$OS" = "android" ]; then
-        ARCHIVE_FULL_NAME="$ARCHIVE_NAME.zip"
-        zip -r "$ARCHIVE_FULL_NAME" "$REV_NAME"
+        ARCHIVE_NAME="${REV_NAME}.zip"
+        # powershell Compress-Archive "$REV_NAME" "$ARCHIVE_NAME"
     else
-        ARCHIVE_FULL_NAME="$ARCHIVE_NAME.tar.gz"
-        tar czvf "$ARCHIVE_FULL_NAME" "$REV_NAME"
+        ARCHIVE_NAME="${REV_NAME}.tar.gz"
+        tar czvf "$ARCHIVE_NAME" "$REV_NAME"
     fi
-    # mv "$ARCHIVE_FULL_NAME" artifacts/
 
-    if [ -z "$SKIP_7Z" ]; then
-        # Create .7z
-        ARCHIVE_FULL_NAME="$ARCHIVE_NAME.7z"
-        mv "$REV_NAME" "$RELEASE_NAME"
-        7z a "$ARCHIVE_FULL_NAME" "$RELEASE_NAME"
-        mv "$ARCHIVE_FULL_NAME" artifacts/
+    mv "$REV_NAME" $RELEASE_NAME
+    7z a "$REV_NAME.7z" $RELEASE_NAME
 
-        # Clean up created release artifacts directory.
-        rm -rf "$RELEASE_NAME"
-    else
-        # Clean up created rev artifacts directory.
-        rm -rf "$REV_NAME"
-    fi
-}
-
-if [ -n "$UNPACKED" ]; then
-    # Copy the artifacts to be uploaded unpacked.
+    # mv "$ARCHIVE_NAME" artifacts/
+    mv "$REV_NAME.7z" artifacts/
+else
+    # Directly upload the raw artifacts, renamed with the revision.
     for ARTIFACT in build/bundle/*; do
         FILENAME=$(basename "$ARTIFACT")
         EXTENSION="${FILENAME##*.}"
-
         mv "$ARTIFACT" "artifacts/$REV_NAME.$EXTENSION"
     done
-elif [ -n "$PACK_INDIVIDUALLY" ]; then
-    # Pack and upload the artifacts one-by-one.
-    for ARTIFACT in build/bundle/*; do
-        pack_artifacts "$ARTIFACT"
-    done
-else
-    # Pack all of the artifacts into a single archive.
-    pack_artifacts build/bundle
 fi
