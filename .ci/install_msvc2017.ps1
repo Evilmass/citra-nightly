19cbc283795c214bf3f246921dd032b96c94cd28
Evilmass <Evilmass@users.noreply.github.com>
2025-08-01 10:28:36 +0800

    component url

--- .ci/install_msvc2017.ps1
+++ .ci/install_msvc2017.ps1
@@ -1,10 +1,12 @@
-# vs2022 -> "Microsoft.VisualStudio.Component.VC.v141.x86.x64"
+# if vs2022 -> /aka.ms/vs/17 -> "Microsoft.VisualStudio.Component.VC.v141.x86.x64"
 $vs_url = "https://aka.ms/vs/15/release/vs_BuildTools.exe"
 Invoke-WebRequest -Uri $vs_url -OutFile "vs_buildtools.exe"
 if (-not (Test-Path "vs_buildtools.exe")) {
 Write-Error "Failed to download VS Build Tools"
 exit 1
 }
+# https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-studio-2017/install/workload-component-id-vs-build-tools?view=vs-2017#msbuild-tools
+# https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-studio-2017/install/workload-component-id-vs-build-tools?view=vs-2017#visual-c-build-tools
 $components = @(
 "Microsoft.VisualStudio.Workload.MSBuildTools",
 "Microsoft.VisualStudio.Workload.VCTools",
