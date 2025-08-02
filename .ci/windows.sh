c97c4cc7cc2dab01c30f60ca1cf45064f9c711a1
Evilmass <21218296+Evilmass@users.noreply.github.com>
2025-08-02 22:45:14 +0800

    cicd use msvc2017

--- .ci/windows.sh
+++ .ci/windows.sh
@@ -1,17 +1,18 @@
 #!/bin/sh -ex
 
-mkdir build && cd build
-cmake .. -G "Visual Studio 17 2022" -A x64 \
-    -DCMAKE_GENERATOR_TOOLSET=v141 \
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+cmake --fresh -S . -B build -G "Visual Studio 17 2022" -A x64 -T v141 \
     -DCMAKE_BUILD_TYPE=Release \
     -DCITRA_USE_BUNDLED_QT=1 \
     -DCITRA_USE_BUNDLED_SDL2=1 \
-    -DENABLE_QT_TRANSLATION=ON \
+    -DENABLE_QT_TRANSLATION=OFF \
     -DCITRA_ENABLE_COMPATIBILITY_REPORTING=OFF \
-    -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON \
-    -DUSE_DISCORD_PRESENCE=ON \
+    -DUSE_DISCORD_PRESENCE=OFF \
     -DENABLE_FFMPEG_VIDEO_DUMPER=ON \
-    -DENABLE_MF=ON
-msbuild build/citra.sln /m /p:Configuration=Release,Platform=x64 /t:Rebuild
-ctest -VV -C Release || echo "::error ::Test error occurred on Windows build"
+    -DENABLE_MF=ON \
+    -DENABLE_LTO=ON
+"C:/BuildTools/MSBuild/Current/Bin/MSBuild.exe" build/citra.sln \
+  -property:Configuration=Release,Platform=x64 \
+  -maxCpuCount \
+  -target:Rebuild
+
+dir build/bin/Release
