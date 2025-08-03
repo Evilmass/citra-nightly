5c93294016f117709e7edcda326cc003e591eeef
Evilmass <21218296+Evilmass@users.noreply.github.com>
2025-08-03 11:33:56 +0800

    fix submodules, build

--- CMakeModules/DownloadExternals.cmake
+++ CMakeModules/DownloadExternals.cmake
@@ -1,18 +1,25 @@
-
 # This function downloads a binary library package from our external repo.
 # Params:
 #   remote_path: path to the file to download, relative to the remote repository root
+#   lib_name: name of the library (used for naming local files)
 #   prefix_var: name of a variable which will be set with the path to the extracted contents
 function(download_bundled_external remote_path lib_name prefix_var)
-set(prefix "${CMAKE_BINARY_DIR}/externals/${lib_name}")
-if (NOT EXISTS "${prefix}")
-    message(STATUS "Downloading binaries for ${lib_name}...")
-    file(DOWNLOAD
-        https://github.com/citra-emu/ext-windows-bin/raw/master/${remote_path}${lib_name}.7z
-        "${CMAKE_BINARY_DIR}/externals/${lib_name}.7z" SHOW_PROGRESS)
-    execute_process(COMMAND ${CMAKE_COMMAND} -E tar xf "${CMAKE_BINARY_DIR}/externals/${lib_name}.7z"
-        WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/externals")
-endif()
-message(STATUS "Using bundled binaries at ${prefix}")
-set(${prefix_var} "${prefix}" PARENT_SCOPE)
+    set(prefix "${CMAKE_BINARY_DIR}/externals/${lib_name}")
+    set(archive "${CMAKE_BINARY_DIR}/externals/${lib_name}.7z")
+    set(url "https://github.com/Evilmass/ext-win-bin/raw/main/${remote_path}${lib_name}.7z")
+
+    # Download archive if missing
+    if(NOT EXISTS "${archive}")
+        message(STATUS "Downloading ${lib_name} from ${url}")
+        file(DOWNLOAD ${url} "${archive}" SHOW_PROGRESS TIMEOUT 300)
+    endif()
+
+    # Extract archive if needed
+    if(NOT EXISTS "${prefix}")
+        message(STATUS "Extracting ${archive} to ${prefix}")
+        execute_process(COMMAND ${CMAKE_COMMAND} -E tar xf "${archive}"
+            WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/externals")
+    endif()
+
+    set(${prefix_var} "${prefix}" PARENT_SCOPE)
 endfunction()
