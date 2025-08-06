568fb965e2fe75c7f6856f6b0a426cfb0f04e32a
Evilmass <Evilmass@users.noreply.github.com>
2025-08-06 18:12:58 +0800

    Revert "Improve core timing accuracy (#5257)"
    This reverts commit 57aa18f52ea35ca74cd1a6c406a4abf04049b44e.

--- src/core/core_timing.h
+++ src/core/core_timing.h
@@ -175,13 +175,7 @@ public:
         BOOST_SERIALIZATION_SPLIT_MEMBER()
     };
 
-    // currently Service::HID::pad_update_ticks is the smallest interval for an event that gets
-    // always scheduled. Therfore we use this as orientation for the MAX_SLICE_LENGTH
-    // For performance bigger slice length are desired, though this will lead to cores desync
-    // But we never want to schedule events into the current slice, because then cores might to
-    // run small slices to sync up again. This is especially important for events that are always
-    // scheduled and repated.
-    static constexpr int MAX_SLICE_LENGTH = BASE_CLOCK_RATE_ARM11 / 234;
+    static constexpr int MAX_SLICE_LENGTH = 20000;
 
     class Timer {
     public:
@@ -190,9 +184,7 @@ public:
 
         s64 GetMaxSliceLength() const;
 
-        void Advance();
-
-        void SetNextSlice(s64 max_slice_length = MAX_SLICE_LENGTH);
+        void Advance(s64 max_slice_length = MAX_SLICE_LENGTH);
 
         void Idle();
 
@@ -239,6 +231,7 @@ public:
         template <class Archive>
         void serialize(Archive& ar, const unsigned int) {
             MoveEvents();
+            // NOTE: ts_queue should be empty now
             ar& event_queue;
             ar& event_fifo_id;
             ar& slice_length;
@@ -276,6 +269,10 @@ public:
 
     s64 GetGlobalTicks() const;
 
+    void AddToGlobalTicks(s64 ticks) {
+        global_timer += ticks;
+    }
+
     /**
      * Updates the value of the cpu clock scaling to the new percentage.
      */
@@ -294,6 +291,8 @@ public:
     static s64 GenerateBaseTicks();
 
 private:
+    s64 global_timer = 0;
+
     // unordered_map stores each element separately as a linked list node so pointers to
     // elements remain stable regardless of rehashes/resizing.
     std::unordered_map<std::string, TimingEventType> event_types = {};
@@ -308,6 +307,7 @@ private:
     template <class Archive>
     void serialize(Archive& ar, const unsigned int file_version) {
         // event_types set during initialization of other things
+        ar& global_timer;
         ar& timers;
         ar& current_timer;
         if (Archive::is_loading::value) {
