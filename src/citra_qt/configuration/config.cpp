d86c923bd0fdc2a5a9ecbf10fbd0965b1ab1cfce
Evilmass <21218296+Evilmass@users.noreply.github.com>
2025-08-09 01:27:27 +0800

    port Android & PC: Implement Raise CPU Ticks option #197
    from https://github.com/azahar-emu/azahar/pull/197/files

--- src/citra_qt/configuration/config.cpp
+++ src/citra_qt/configuration/config.cpp
@@ -454,6 +454,7 @@ void Config::ReadCoreValues() {
     qt_config->beginGroup(QStringLiteral("Core"));
 
     ReadGlobalSetting(Settings::values.cpu_clock_percentage);
+    ReadGlobalSetting(Settings::values.raise_cpu_ticks);
 
     if (global) {
         ReadBasicSetting(Settings::values.use_cpu_jit);
@@ -992,6 +993,7 @@ void Config::SaveCoreValues() {
     qt_config->beginGroup(QStringLiteral("Core"));
 
     WriteGlobalSetting(Settings::values.cpu_clock_percentage);
+    WriteGlobalSetting(Settings::values.raise_cpu_ticks);
 
     if (global) {
         WriteBasicSetting(Settings::values.use_cpu_jit);
