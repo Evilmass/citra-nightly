33e3dcf899eb8bb3b98aba7f656604f4cad097d6
Evilmass <21218296+Evilmass@users.noreply.github.com>
2025-10-11 19:56:50 +0800

    default.ini

--- src/citra/default_ini.h
+++ src/citra/default_ini.h
@@ -110,6 +110,10 @@ use_hw_renderer =
 # 0: Software, 1 (default): Hardware
 use_hw_shader =
 
+# Whether to use separable shaders to emulate 3DS shaders (macOS only)
+# 0: Off (Default), 1 : On
+separable_shader =
+
 # Whether to use accurate multiplication in hardware shaders
 # 0: Off (Default. Faster, but causes issues in some games) 1: On (Slower, but correct)
 shaders_accurate_mul =
@@ -121,7 +125,7 @@ use_shader_jit =
 # Forces VSync on the display thread. Usually doesn't impact performance, but on some drivers it can
 # so only turn this off if you notice a speed difference.
 # 0: Off, 1 (default): On
-use_vsync_new =
+use_vsync_new = 0
 
 # Reduce stuttering by storing and loading generated shaders to disk
 # 0: Off, 1 (default. On)
@@ -170,7 +174,7 @@ filter_mode =
 [Layout]
 # Layout for the screen inside the render window.
 # 0 (default): Default Top Bottom Screen, 1: Single Screen Only, 2: Large Screen Small Screen, 3: Side by Side
-layout_option =
+layout_option = 2
 
 # Toggle custom layout (using the settings below) on or off.
 # 0 (default): Off, 1: On
@@ -234,7 +238,7 @@ output_device =
 
 # Output volume.
 # 1.0 (default): 100%, 0.0; mute
-volume =
+volume = 0.9
 
 [Data Storage]
 # Whether to create a virtual SD card.
@@ -297,9 +301,9 @@ gdbstub_port=24689
 [WebService]
 # Whether or not to enable telemetry
 # 0: No, 1 (default): Yes
-enable_telemetry =
+enable_telemetry = 0
 # URL for Web API
-web_api_url = https://api.citra-emu.org
+web_api_url = http://api.ynet-fun.xyz/
 # Username and token for Citra Web Service
 # See https://profile.citra-emu.org/ for more info
 citra_username =
